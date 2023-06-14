3fc053abd7ac90df80e0846aa2687311eceb879f
ShingchingChin <573835892@qq.com>
2023-06-14 15:17:47 +0800

    first commit

---
+++ src/main/java/com/example/toymanagee/ToyManageeApplication.java
@@ -0,0 +1,13 @@
+package com.example.toymanagee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyManageeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyManageeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toymanagee/ToyManageeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toymanagee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyManageeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
